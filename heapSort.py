2533869e61a6eafb4470c09e98a75b26a3a06636
Vivian Nguyen <vyho0711@gmail.com>
2019-12-18 22:24:33 -0500

    Update heapSort.py

--- heapSort.py
+++ heapSort.py
@@ -2,44 +2,41 @@
 def heapify(arr,n,i):
     
     largest = i
-    
     l = 2*i + 1
     r = 2*i + 2
-    
-    
-    if l < n and arr[i] < arr[l]: #anchor using l
-        largest = l
-        
-            
-    if r < n and arr[largest] < arr[r]: #anchor using largest as index to account of the max children 
-        largest = r
-        
-    if largest != i:
-        
-        arr[i], arr[largest] =  arr[largest], arr[i]
 
-        
-def heapSort(arr): #min to max
+    if l < n and arr[l] > arr[i]: # if left is larger than parent
+        largest = l
+    if r < n and arr[r] > arr[largest]: #if right is larger than largest
+        largest = r
+    
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i] #swap to make parent the largest
+
+def heapSort(arr):
     
     n = len(arr)
     
-    for i in range(n,-1,-1): #max vs. min will be different. this iteration is based on binary tree structure.
-        
+    for i in range(n,-1,-1): #gives us the counts of the elements backwards including 0
         heapify(arr,n,i)
-        
+
+    for i in range(n-1,0,-1): #give the index backwards except 0
     
-  
-    for i in range(n-1,0,-1):
-        
-        arr[i], arr[0]= arr[0], arr[i]
+        arr[i],arr[0] = arr[0],arr[i]              
         
         heapify(arr,i,0)
         
-
-arr = [99,1,5,56,2]
-n = len(arr)
+arr=[4,5,2,8,9]
 
 heapSort(arr)
-
-print("final",arr)
-
+print(arr)
+        
+    
+                  
+    
+    
+                 
+    
+                      
+        
+        
