fd786df9c14074ccbf3e6115ea0a6d6499d09ddb
Vivian Nguyen <vyho0711@gmail.com>
2019-12-21 16:08:06 -0500

    Create quickSort.py

---
+++ quickSort.py
@@ -0,0 +1,34 @@
+
+def quicksort(array):
+    
+    less = []
+    equal = []
+    greater = []
+    
+    if len(array) > 1:
+        pivot = array[0]
+        
+        for i in array:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                greater.append(i)
+            else:
+                equal.append(i)
+                
+        return sorted(less) + equal + sorted(greater)
+    else: #for when array has only 1 element
+        return array
+
+array = [1,5,7,3,8]
+
+result = quicksort(array)
+
+print(result)
+
+
+from timeit import Timer
+
+t = Timer("quicksort(array)")
+
+print(t)
