95b53a04d08edb0cf0ce09dd53c9fcec6d76f086
Vivian Nguyen <vyho0711@gmail.com>
2019-12-23 23:42:47 -0500

    Update quickSort.py

--- quickSort.py
+++ quickSort.py
@@ -1,21 +1,41 @@
-def partitionfinal(array, begin, end):
-    pivot = begin
-    for i in range(begin+1, end+1):
-        if array[i] <= array[begin]:
-            pivot += 1
-            array[i], array[pivot] = array[pivot], array[i]
-    array[pivot], array[begin] = array[begin], array[pivot]
-    return pivot
 
+def partition(arr,begin,end):
+    
+   
+           
+    for i in range(begin,end):
+       
+        if arr[i] > arr[end]:
+            arr.insert(end+1,arr[i])
+            del arr[i]
+            
+            
+    return arr    
 
+def quick(arr):
+    
+    pivot = None
+    
+    if pivot is None:
+        
+       
+        begin = 0
+        end = len(arr)-1
+        pivot = arr[end]
+        
+        while len(arr[begin:end+1])> 1:  
+            
+            
+         
+            
+            partition(arr,0,end)
+            
+            
+            
+                    
+    
+             
+             
+    
 
-def quicksort(array, begin=0, end=None):
-    if end is None:
-        end = len(array) - 1
-    def _quicksort(array, begin, end):
-        if begin >= end:
-            return
-        pivot = partitionfinal(array, begin, end)
-        _quicksort(array, begin, pivot-1)
-        _quicksort(array, pivot+1, end)
-    return _quicksort(array, begin, end)
+    return arr
