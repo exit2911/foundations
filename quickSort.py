90083365a40ae6519ed39bfc7f31b834c792f702
Vivian Nguyen <vyho0711@gmail.com>
2019-12-26 23:23:14 -0500

    Update quickSort.py

--- quickSort.py
+++ quickSort.py
@@ -1,41 +1,36 @@
 
-def partition(arr,begin,end):
+def partition(arr,low,high):
     
-   
+    i = low - 1
+    pivot = arr[high]
+    
+    for j in range(low,high):
+        
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
            
-    for i in range(begin,end):
-       
-        if arr[i] > arr[end]:
-            arr.insert(end+1,arr[i])
-            del arr[i]
             
-            
-    return arr    
+    arr[i+1], arr[high] = arr[high], arr[i+1] #high is right of the last smallest item
+    return i + 1
 
-def quick(arr):
-    
-    pivot = None
-    
-    if pivot is None:
-        
-       
-        begin = 0
-        end = len(arr)-1
-        pivot = arr[end]
-        
-        while len(arr[begin:end+1])> 1:  
-            
-            
-         
-            
-            partition(arr,0,end)
-            
-            
-            
-                    
-    
-             
-             
-    
 
-    return arr
+
+def quickSort(arr,low,high):
+    count = 0
+    if low < high:
+        count += 1
+       
+        pi = partition(arr,low,high)
+        quickSort(arr,low,pi-1)
+        quickSort(arr,pi+1,high)
+        print(count)
+    
+arr = [1,2,22,11,0,99,3,45]
+      
+n = len(arr)-1
+       
+quickSort(arr,0,n)
+
+print(arr)
+
