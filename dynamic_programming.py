5939be0cec693329359276cb4f9e27eec7366799
Vivian Nguyen <vyho0811@gmail.com>
2020-02-15 23:27:38 -0500

    Create dynamic_programming.py

---
+++ dynamic_programming.py
@@ -0,0 +1,42 @@
+"""
+dynamic programming 
+
+breaks problem into smaller sub problems and solve
+
+mostly solved in 2 ways
+
+
+
+"""
+
+fib_cache = {}
+
+def memo_fib(n): #memorize , top down
+    
+  global fib_cache
+  if n == 0 or n == 1:
+     return 1
+ 
+  if n in fib_cache:
+     return fib_cache[n]
+ 
+  ret = memo_fib(n - 1) + memo_fib(n - 2)
+  
+  fib_cache[n] = ret
+  
+  return ret
+
+
+
+
+def dp_fib(n): #tabulation , bottom up
+    
+   partial_answers = [1, 1]
+   
+   while len(partial_answers) <= n:
+   
+       partial_answers.append(partial_answers[-1] + partial_answers[-2])
+   
+    return partial_answers[n]
+
+print(memo_fib(5), dp_fib(5))
